8fcf7702ab7c73a98e141a8088fc25fc7b002282
ekamp <ekamp@eden.rutgers.edu>
2015-05-18 19:18:00 -0400

    Adding in facebook status check as to retrieve the feed the user needs to approve our application on their facebook

--- js/auequeFacebook.js
+++ js/auequeFacebook.js
@@ -5,11 +5,24 @@
           version    : 'v2.3'
         });
 
+        //Determines the current user's login status
         FB.getLoginStatus(function(response){
-          getUserFeed();
+        	//User has approved our application
+        	if(response.status == 'connected'){
+        		var uid = response.authResponse.userID;
+    			var accessToken = response.authResponse.accessToken;
+        		getUserFeed();		
+        	} else if(response.status == 'not_authorized'){
+        		//Ask the user to approve the application
+        		authenticateFacebookUser();
+        	} else {
+        		//Ask the user to login to facebook
+        		authenticateFacebookUser()
+        	}
         });
       };
 
+      //Grabs the Facebook javascript code
       (function(d, s, id){
          var js, fjs = d.getElementsByTagName(s)[0];
          if (d.getElementById(id)) {return;}
@@ -18,8 +31,23 @@
          fjs.parentNode.insertBefore(js, fjs);
        }(document, 'script', 'facebook-jssdk'));
 
+    // If a user is not authenticated and we wish to get the facebook feed we must call this function so the user 
+    // can approve our application
+	function authenticateFacebookUser(){
+		FB.login(function(response) {
+	   		if (response.authResponse) {
+	     		console.log('Welcome!  Fetching your information.... ');
+	     		FB.api('/me', function(response) {
+	       			console.log('Good to see you, ' + response.name + '.');
+	     		});
+	   		} else {
+	     		console.log('User cancelled login or did not fully authorize.');
+	   		}
+ 		});
+    }
 
-    function getUserFeed(){
+    //Retrieves the feed for facebook.com/aueuque
+	function getUserFeed(){
         FB.api(
         "/271380496376837/feed",
         function (response) {
